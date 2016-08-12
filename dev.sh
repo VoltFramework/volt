2e6663ddbdd7837d91313fa5c1d5497c45413275
Victor Vieux <victorvieux@gmail.com>
2016-08-12 04:21:11 -0700

    remove dev.sh

--- dev.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-docker run -p 5050:5050 --name master -d jimenez/mesos-master --ip=0.0.0.0 --work_dir=/ --registry=in_memory
-docker run -p 5051:5051 --name slave --link=master:master -v /sys/fs/cgroup/:/sys/fs/cgroup/ -v /usr/bin/docker:/usr/bin/docker -v /var/run/docker.sock:/var/run/docker.sock -d jimenez/mesos-slave --master=master:5050 --containerizers=docker,mesos --hostname="198.27.68.58"
